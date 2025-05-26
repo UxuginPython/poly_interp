d6764224870ee712f7a8313e656ce5439b715a9c
Uxugin <git@uxugin.com>
2025-05-26 17:57:39 -0600

    derivative test

---
+++ tests/tests.rs
@@ -0,0 +1,11 @@
+use poly_interp::*;
+#[test]
+fn derivative() {
+    //4x^3+3x^2+2x+1
+    let polynomial = Polynomial::new(vec![1.0, 2.0, 3.0, 4.0]);
+    //12x^2+6x+2
+    assert_eq!(
+        polynomial.derivative(),
+        Polynomial::new(vec![2.0, 6.0, 12.0])
+    );
+}
