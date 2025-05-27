bfe0ab2533311ecd2a87b0e3720969a1a3b5b5b0
Uxugin <git@uxugin.com>
2025-05-26 18:00:06 -0600

    evaluate test

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,5 +1,11 @@
 use poly_interp::*;
 #[test]
+fn evaluate() {
+    //4x^3+3x^2+2x+1
+    let polynomial = Polynomial::new(vec![1.0, 2.0, 3.0, 4.0]);
+    assert_eq!(polynomial.evaluate(5.0), PointXY::new(5.0, 586.0));
+}
+#[test]
 fn derivative() {
     //4x^3+3x^2+2x+1
     let polynomial = Polynomial::new(vec![1.0, 2.0, 3.0, 4.0]);
